6e6038fb5b0310f113b0cc2d4c503a26ded2decb
Josh Saterdalen <josh@saterdalen.me>
2022-03-14 16:04:04 -0500

    clean up formatting

--- pypado.py
+++ pypado.py
@@ -1,33 +1,37 @@
 #! /usr/bin/env python3
 
+"""Format a given SFDX package.xml file for importing into a Copado Work Item.
+"""
+
+import os.path
 import sys
 from bs4 import BeautifulSoup
 import pyperclip
-import os.path
 
 if len(sys.argv) > 1:
-    dxPackage = sys.argv[1]
+    PACKAGE_XML = sys.argv[1]
 elif os.path.exists('package.xml'):
-    dxPackage = 'package.xml'
+    PACKAGE_XML = 'package.xml'
 else:
-    print("No package.xml found. Either run in the same directory as a package.xml, or pass it e.g. 'pypado manifest/package.xml'")
+    print("No package.xml found. Either run in the same directory as a \
+        package.xml, or pass it e.g. 'pypado manifest/package.xml'")
 
-with open(os.path.expanduser(dxPackage), 'r') as f:
+with open(os.path.expanduser(PACKAGE_XML), 'r') as f:
     file = f.read()
 soup = BeautifulSoup(file, 'xml')
 
 types = soup.find_all('types')
-componentList = []
+component_list = []
 
-for type in types:
-    mdtType = type.find('name')
-    mdtNames = type.find_all('members')
-    for mdtName in mdtNames:
-        # print(mdtType.string + "/" + mdtName.string)
-        componentList.append(f"{mdtType.string}/{mdtName.string}")
+for t in types:
+    mdt_type = t.find('name')
+    mdt_names = t.find_all('members')
+    for mdt_name in mdt_names:
+        # print(mdt_type.string + "/" + mdt_name.string)
+        component_list.append(f"{mdt_type.string}/{mdt_name.string}")
 
-compListFormatted = "\n".join(componentList)
-pyperclip.copy(compListFormatted)
+COMP_LIST_FORMATTED = "\n".join(component_list)
+pyperclip.copy(COMP_LIST_FORMATTED)
 
-print('\n' + compListFormatted + '\n')
+print('\n' + COMP_LIST_FORMATTED + '\n')
 print("Component list has been copied to your clipboard.")
