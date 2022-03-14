b1d44782ea2f989018a88141e447dbe4530a8ce9
Josh Saterdalen <josh@saterdalen.me>
2022-03-14 13:12:40 -0500

    format components for copado import

--- copado-format.py
+++ copado-format.py
@@ -7,22 +7,10 @@ with open('package.xml', 'r') as f:
 
 soup = BeautifulSoup(file, 'xml')
 
-# members = soup.find_all('members')
-# for member in members:
-#     print(member.text)
-#     print(member.parent)
-
-# find all types children
-# for each child print name/member
-
-# type = soup.find('types')
-# compName = type.find("name")
-# print(compName)
-
 types = soup.find_all('types')
 
 for type in types:
     mdtType = type.find("name")
     mdtNames = type.find_all("members")
     for mdtName in mdtNames:
-        print(mdtName.string)
+        print(mdtType.string + "/" + mdtName.string)
